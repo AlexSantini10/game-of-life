c7def75df99f7799adf39c8b585a8b5ac75f6df3
Alex Santini <47783526+AlexSantini10@users.noreply.github.com>
2021-01-02 21:35:22 +0100

    added mobile configuration

--- script.js
+++ script.js
@@ -15,12 +15,22 @@ document.addEventListener('DOMContentLoaded', () => {
  
     var canvas = document.createElement('canvas');
     canvas.id = "canvas";
-    canvas.height = (window.screen.height/2)-(window.screen.height/2)%dim;
-    canvas.width = (window.screen.width/2)-(window.screen.width/2)%dim;
+
+    if (window.screen.height <= window.screen.width){
+        canvas.height = (window.screen.height/2)-(window.screen.height/2)%dim;
+        canvas.width = (window.screen.width/2)-(window.screen.width/2)%dim;
+        canvas.style.marginTop = String((window.screen.height/2)-(canvas.height/100*75)) + 'px';
+        canvas.style.marginLeft = String((window.screen.width/2)-(canvas.width/2)) + 'px';
+    }
+    else {
+        canvas.height = (window.screen.height/100*80)-(window.screen.height/100*80)%dim;
+        canvas.width = (window.screen.width/100*90)-(window.screen.width/100*90)%dim;
+        canvas.style.marginTop = String((window.screen.height/2)-(canvas.height/100*60)) + 'px';
+        canvas.style.marginLeft = String((window.screen.width/2)-(canvas.width/100*50)) + 'px';
+    }
+    
     canvas.style.zIndex = 8;
     canvas.style.position = "absolute";
-    canvas.style.marginTop = String((window.screen.height/2)-(canvas.height/100*75)) + 'px';
-    canvas.style.marginLeft = String((window.screen.width/2)-(canvas.width/2)) + 'px';
     canvas.style.border = '1px solid #EDEDED';
     canvas.style.borderRadius = '3px';
 
@@ -59,8 +69,18 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    canvas.addEventListener("touchmove", function (e) {
+        let touch = e.touches[0];
+
+        pos.x = touch.clientX; pos.x -= pos.x%dim;
+        pos.y = touch.clientY; pos.y -= pos.y%dim;
+
+        makeAlive(pos.y/dim, pos.x/dim);
+        reDrawCanva(rows, cols);
+    });
+
     canvas.addEventListener('mouseenter', (e) => {
-        pos = getCursorPosition(canvas, e)
+        pos = getCursorPosition(canvas, e);
         
         pos.x -= pos.x%dim;
         pos.y -= pos.y%dim;
